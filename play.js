08200865b49bc2cfce96383373c0ca3f49cd3917
nasouh94 <nasouh@live.com>
2019-09-25 18:17:48 +0000

    created a new file input.js and refactored play.js by moving all setupinput function into the unput.js file

--- play.js
+++ play.js
@@ -1,28 +1,7 @@
 const { connect }  = require("./client");
+const { setupInput } = require("./input")
 
 console.log("Connecting...");
 connect();
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data" , handleUserInput)
-  return stdin;
-  
-}
-
-function handleUserInput(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-
-}
-
 setupInput()
 
