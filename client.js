74daece7c79531b9437c434d9254180d6c296e31
nasouh94 <nasouh@live.com>
2019-09-25 19:36:58 +0000

    moved the ip and host into a seprate file called constants

--- client.js
+++ client.js
@@ -1,24 +1,15 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: `192.168.0.103`,
-    port: 50541
+    host: IP,
+    port: PORT
   });
   
 conn.on("connect",() => {
   console.log("Successfully connected to game server" )
   conn.write("Name: Nas");
-  // conn.write("Move: up");
-  // conn.write("Move: down");
-  // conn.write("Move: left");
-  // conn.write("Move: right");
-
-  // setInterval(() => {
-  //   conn.write("Move: up");
-  // }, 100);
-
-  
 });
 
 
